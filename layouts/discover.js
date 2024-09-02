8c8198ab4befb01bdce4fb74d17390cbe697f4ed
heyloura <112587406+heyloura@users.noreply.github.com>
2024-09-02 09:21:33 -0400

    Update discover.js

--- layouts/discover.js
+++ layouts/discover.js
@@ -5,6 +5,17 @@ const _discoverTemplate = new TextDecoder().decode(await Deno.readFile("template
 const _ctaTemplate = new TextDecoder().decode(await Deno.readFile("templates/_cta.html"));
 const _loginTemplate = new TextDecoder().decode(await Deno.readFile("templates/_login.html"));
 
+function tryParseJSONObject (jsonString){
+    try {
+        var o = JSON.parse(jsonString);
+        if (o && typeof o === "object") {
+            return o;
+        }
+    }
+    catch (e) { }
+    return false;
+};
+
 export async function DiscoverTemplate(user, token, uuid, url) {
     url = url.replaceAll('/discover','');
     if(url[url.length - 1] == '/') {
@@ -13,7 +24,12 @@ export async function DiscoverTemplate(user, token, uuid, url) {
 
     const config = token ? { method: "GET", headers: { "Authorization": "Bearer " + token } } : { method: "GET", headers: { "Authorization": "Bearer " + token }  };
     const fetching = await fetch('https://micro.blog/posts/discover', config);
-    const results = await fetching.json();
+    let results;
+    if(tryParseJSONObject(fetching.text())){
+        results = await fetching.json();
+    } else {
+        return HTMLPage(`Discover`, `<p>Micro.blog did not return results.</p>`, user);
+    }
 
     const tagmojis = results._microblog.tagmoji.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).map((item) =>
         `<span class="chip"><a onclick="addLoading(this)" href="/discover/${item.name}">${item.emoji} ${item.title}</a></span>`
