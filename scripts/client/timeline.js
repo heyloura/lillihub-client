80f5d6645097d739f4169936503b895ae9c6d593
heyloura <112587406+heyloura@users.noreply.github.com>
2024-11-27 13:03:47 -0500

    remove hash

--- scripts/client/timeline.js
+++ scripts/client/timeline.js
@@ -26,12 +26,26 @@
 
 
 if(window.location.hash) {
-    window.location.hash = '';
+    removeHash();
 }
 
 
+function removeHash() { 
+    var scrollV, scrollH, loc = window.location;
+    if ("pushState" in history)
+        history.pushState("", document.title, loc.pathname + loc.search);
+    else {
+        // Prevent scrolling by storing the page's current scroll offset
+        scrollV = document.body.scrollTop;
+        scrollH = document.body.scrollLeft;
 
+        loc.hash = "";
 
+        // Restore the scroll offset, should be flicker free
+        document.body.scrollTop = scrollV;
+        document.body.scrollLeft = scrollH;
+    }
+}
 function growTextArea(el) {
     el.parentNode.dataset.replicatedValue = el.value;
 }
