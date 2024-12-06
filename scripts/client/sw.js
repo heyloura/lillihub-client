e69b35264937768dd8962d31f5e8895d1b7b5a3d
heyloura <112587406+heyloura@users.noreply.github.com>
2024-12-06 13:20:59 -0500

    sigh...

--- scripts/client/sw.js
+++ scripts/client/sw.js
@@ -1,4 +1,4 @@
-const version = '0.0.68';
+const version = '0.0.69';
 const url = ''
 
 const coreID = `${version}_core`;
@@ -103,20 +103,16 @@ self.addEventListener('fetch', async function (event) {
             try {
                 const response = await fetch(event.request);
                 var copy = response.clone();
-                var headers = new Headers(copy.headers);
-                headers.append('sw-fetched-on', new Date().getTime());
-                var body = await copy.blob();
+                // var headers = new Headers(copy.headers);
+                // headers.append('sw-fetched-on', new Date().getTime());
+                // var body = await copy.blob();
                 
                 if (event.request.headers.get('Accept').includes('image')) {
                     let cache = await caches.open(imageID);
                     await cache.put(event.request, copy);
                 } else if (event.request.destination) {
                     let cache = await caches.open(pageID);
-                    await cache.put(event.request, new Response(body, {
-                        status: copy.status,
-                        statusText: copy.statusText,
-                        headers: headers
-                    }));
+                    await cache.put(event.request, copy);
                 }
 
                 return response;
