a8da912b1ddc9c8a3fa1335c6761fcbe13fdc344
heyloura <112587406+heyloura@users.noreply.github.com>
2024-12-05 19:27:34 -0500

    48

--- scripts/client/sw.js
+++ scripts/client/sw.js
@@ -1,4 +1,4 @@
-const version = '0.0.47';
+const version = '0.0.48';
 const url = ''
 
 const coreID = `${version}_core`;
@@ -109,11 +109,11 @@ self.addEventListener('fetch', async function (event) {
                 if (event.request.headers.get('Accept').includes('image')) {
                     let cache = await caches.open(imageID);
                     await cache.put(event.request, copy);
-                } else {
-                    self.console.log('not caching: ');
-                    self.console.log(event.request);
-                    //let cache = await caches.open(pageID);
-                    //await cache.put(event.request, copy);
+                } else if (event.request.destination) {
+                    //self.console.log('not caching: ');
+                    //self.console.log(event.request);
+                    let cache = await caches.open(pageID);
+                    await cache.put(event.request, copy);
                 }
 
                 return response;
