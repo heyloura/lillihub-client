cb5065b4997ca26a2326d9b96773bcc98427ee60
heyloura <112587406+heyloura@users.noreply.github.com>
2024-12-11 09:08:41 -0500

    magic grid

--- scripts/client/common.js
+++ scripts/client/common.js
@@ -354,26 +354,20 @@ async function loadNotebook() {
         element.innerHTML = html;
     });
 
-    // Promise.all(promises)
-    // .then(() => {
-    //     hljs.highlightAll();
-    //     const macyInstance = Macy({
-    //         container: '#note-list',
-    //         trueOrder: false,
-    //         waitForImages: false,
-    //         margin: 8,
-    //         columns: 3,
-    //         breakAt: {
-    //             1200: 2,
-    //             940: 2,
-    //             520: 1,
-    //             400: 1
-    //         }
-    //     });
-    // })
-    // .catch((error) => {
-    //     console.error('Error decrypting elements:', error);
-    // });
+    Promise.all(promises)
+    .then(() => {
+        hljs.highlightAll();
+        let magicGrid = new MagicGrid({
+            container: ".note-container", // Required. Can be a class, id, or an HTMLElement.
+            static: true, // Required for static content.
+            animate: true, // Optional.
+        });
+
+        magicGrid.listen();
+    })
+    .catch((error) => {
+        console.error('Error decrypting elements:', error);
+    });
 }
 
 // Loaded the note
