3d7e75649baec4a140e74199340525d9da7b5710
heyloura <112587406+heyloura@users.noreply.github.com>
2024-12-11 11:38:36 -0500

    css

--- scripts/server/utilities.js
+++ scripts/server/utilities.js
@@ -313,7 +313,7 @@ export function postHTML(post, stranger, isConvo, convoId) {
     
     return ` 
         ${isConvo ? `<div class="timeline-item bordered">
-                        <div class="timeline-left">
+                        <div class="timeline-left ${convoId && convoId === post.id ? 'highlight' : ''}">
                             <span class="timeline-icon"></span>
                         </div>` : ''}
         <article id="post-${post.id}" data-id="${post.id}" class="card parent ${isConvo ? 'timeline-content pt-0 mt-0' : 'bordered'} ${convoId && convoId === post.id ? 'highlight' : ''}" data-reply="${post.username}" data-avatar="${post.avatar}" data-id="${post.id}" data-processed="false" data-url="${post.url}" data-mention="${post.mention}" data-conversation="${post.conversation}" data-timestamp="${post.timestamp}" data-published="${post.published}" data-deletable="${post.deletable}" data-linkpost="${post.linkpost}" data-bookmark="${post.bookmark}" data-favorite="${post.favorite}">
@@ -345,7 +345,7 @@ export function postHTML(post, stranger, isConvo, convoId) {
                     </div>
                 </div>
             </header>
-            <main  class="card-header pl-1 pr-1" id="main-${post.id}" data-id="${post.id}">${post.content}</main>
+            <main id="main-${post.id}" data-id="${post.id}">${post.content}</main>
             ${multipleImgs ? `<div data-id="${post.id}" class='gallery'></div>` : ''}
         </article>
         ${isConvo ? `</div>` : '' }
