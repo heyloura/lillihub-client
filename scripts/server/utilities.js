95bbd2822eac721c15b025d7af728da59be45377
heyloura <112587406+heyloura@users.noreply.github.com>
2024-12-31 17:34:48 -0500

    hmmm

--- scripts/server/utilities.js
+++ scripts/server/utilities.js
@@ -498,7 +498,7 @@ function blogHTML(post) {
                     ${b.name}
                 </a> ` : ``}                      
             <main class="card-subtitle p-0 pt-2 pb-2 markdown ${b.name ? "hasTitle" : ""} ${b.content.includes('<img') ? "hasImages" : ""}">${b.content}</main>
-            <div class="card-footer"><a target="_blank" href="${b.url}">${b.published}</a>
+            <div class="card-footer pl-0"><a target="_blank" href="${b.url}">${b.published.split('T')[0]}</a>
             ${b.category ? b.category.sort().map((item) =>
                 `#${item} `
             ).join('') : ''}</div>      
