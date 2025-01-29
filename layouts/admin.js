91f181c6f82f904c92e5a007886b90fcf1bf1714
heyloura <112587406+heyloura@users.noreply.github.com>
2025-01-29 18:12:30 -0500

    seperate tables

--- layouts/admin.js
+++ layouts/admin.js
@@ -5,18 +5,42 @@ export async function AdminTemplate(user, token) {
     const kv = await Deno.openKv();
     const allEntries = await Array.fromAsync(kv.list({prefix:[]}));
 
-    let results = '';
+    let global = '';
+    let timeline = '';
+    let conversation = '';
+    let old = '';
     for (const entry of allEntries) {
-        results += `<tr>
-            <td><input type="checkbox" name="entry[]" value="${JSON.stringify(entry.key)}" /></td>
-            <td>${entry.key[0]}</td>
-            <td>${entry.key[1]}</td>
-            <td>${JSON.stringify(entry.value.viewed)}</td>
-            <td>${JSON.stringify(entry.value.favorites)}</td>
-            <td>${JSON.stringify(entry.value.feed)}</td>
-            <td>${JSON.stringify(entry.value.display)}</td>
-        </tr>`;
+        if(entry.key[1] == "timeline") {
+            timeline += `<tr>
+                <td><input type="checkbox" name="entry[]" value="${JSON.stringify(entry.key)}" /></td>
+                <td><a href="/user/${entry.key[0]}">${entry.key[0]}</a></td>
+                <td>${JSON.stringify(entry.value.viewed)}</td>
+            </tr>`;
+        } else if(entry.key[1] == "conversation") {
+            conversation += `<tr>
+                <td><input type="checkbox" name="entry[]" value="${JSON.stringify(entry.key)}" /></td>
+                <td><a href="/user/${entry.key[0]}">${entry.key[0]}</a></td>
+                <td>${JSON.stringify(entry.value.viewed)}</td>
+            </tr>`;
+        } else if(entry.key[1] == "global") {
+            global += `<tr>
+                <td><input type="checkbox" name="entry[]" value="${JSON.stringify(entry.key)}" /></td>
+                <td><a href="/user/${entry.key[0]}">${entry.key[0]}</a></td>
+                <td>${JSON.stringify(entry.value.favorites)}</td>
+                <td>${JSON.stringify(entry.value.feed)}</td>
+                <td>${JSON.stringify(entry.value.display)}</td>
+            </tr>`;
+        } else {
+            old += `<tr>
+                <td><input type="checkbox" name="entry[]" value="${JSON.stringify(entry.key)}" /></td>
+                <td><a href="/user/${entry.key[0]}">${entry.key[0]}</a></td>
+            </tr>`;
+        }
+
     }
 
-    return HTMLPage(token, `Admin`, `<table class="table">${results}</table>`, user);
+    return HTMLPage(token, `Admin`, `<h1>Timeline</h1><table class="table">${timeline}</table>
+        <h1>Global</h1><table class="table">${global}</table>
+        <h1>Conversation</h1><table class="table">${conversation}</table>
+        <h1>Old</h1><table class="table">${old}</table>`, user);
 }
