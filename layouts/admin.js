33548fd3fd9c8b6ad2d23970d9b1e8864e182862
heyloura <112587406+heyloura@users.noreply.github.com>
2025-01-29 17:05:19 -0500

    fix.

--- layouts/admin.js
+++ layouts/admin.js
@@ -10,5 +10,5 @@ export async function AdminTemplate(user, token) {
         results += `<li>${entry.key}: ${entry.value}</li>`;
     }
 
-    return HTMLPage(token, `Admin`, `<ul>${results}</ul>`);
+    return HTMLPage(token, `Admin`, `<ul>${results}</ul>`, user);
 }
