9bc24e86be4f55aece8d725a17c2d106749ef520
heyloura <112587406+heyloura@users.noreply.github.com>
2025-01-29 13:52:57 -0500

    tweak to colors

--- layouts/notebooks.js
+++ layouts/notebooks.js
@@ -1,19 +1,19 @@
 import { HTMLPage } from "./templates.js";
 
 const _notebooksTemplate = new TextDecoder().decode(await Deno.readFile("templates/notebooks.html"));
-const colors = ["green-text","greenblue-text", "blue-text", "bluepurple-text", "purple-text", "purplered-text", "red-text", "redorange-text", "orange-text", "orangeyellow-text", "yellow-text", "yellowgreen-text"];
-const borderColors = ["green-border","greenblue-border", "blue-border", "bluepurple-border", "purple-border", "purplered-border", "red-border", "redorange-border", "orange-border", "orangeyellow-border", "yellow-border", "yellowgreen-border"];
+const colors = ["green-text","greenblue-text", "blue-text", "bluepurple-text", "purple-text", "purplered-text", "red-text", "redorange-text", "orange-text", "orangeyellow-text", "yellowgreen-text"];
+const borderColors = ["green-border","greenblue-border", "blue-border", "bluepurple-border", "purple-border", "purplered-border", "red-border", "redorange-border", "orange-border", "orangeyellow-border", "yellowgreen-border"];
 
 export async function NotebooksTemplate(user, token) {
     const fetching = await fetch(`https://micro.blog/notes/notebooks`, { method: "GET", headers: { "Authorization": "Bearer " + token } } );
     const results = await fetching.json();
 
     const notebooks = results.items.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0)).map((item,i) => {
-        return `<li><a class="btn btn-link ${colors[i%12]}" onclick="addLoading(this)" href="/notes/${item.id}">${item.title}</a></li>`;
+        return `<li><a class="btn btn-link ${colors[i%11]}" onclick="addLoading(this)" href="/notes/${item.id}">${item.title}</a></li>`;
     }).join('');
 
     const content = _notebooksTemplate
         .replaceAll('{{notebooks}}', notebooks)
 
-    return HTMLPage('Notebooks', content, user, '', content);
+    return HTMLPage('Notebooks', content, user, '', notebooks);
 }
