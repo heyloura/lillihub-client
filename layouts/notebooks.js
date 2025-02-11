60edf5b25de250711616424dd22b2c60610e6f9a
Loura <loura@hey.com>
2025-02-11 17:12:45 -0500

    menu position fix

--- layouts/notebooks.js
+++ layouts/notebooks.js
@@ -10,13 +10,13 @@ export async function NotebooksTemplate(user, token) {
     const notebooks = results.items.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0)).map((item,i) => {
         return `
         <li>
-            <a class="btn text-left btn-link ${colors[i%11]}" onclick="addLoading(this)" href="/notes/${item.id}">${item.title}</a>
             <div class="dropdown"><button class="btn btn-link dropdown-toggle" tabindex="0"><i class="bi bi-three-dots-vertical"></i></button>
                 <ul class="menu text-left">
                     <li class="menu-item"><a onclick="rename(${item.id},'${item.title}')" href="javascript:void(0)">Rename</a></li>
                     <li class="menu-item"><a onclick="if(confirm('Are you sure you want to delete this notebook? It will also delete <b>all</b> notes this notebook contains. This action cannot be undone.')){deleteConfirmed(${item.id})}" class="red-text" href="javascript:void(0)">Delete</a></li>
                 </ul>
             </div>
+            <a class="btn text-left btn-link ${colors[i%11]}" onclick="addLoading(this)" href="/notes/${item.id}">${item.title}</a>
         </li>`;
     }).join('');
 
