1b93a7c70ee609cd0c00ed303e6ef5129b308377
heyloura <112587406+heyloura@users.noreply.github.com>
2025-05-13 15:20:11 -0400

    hmmmm

--- main.js
+++ main.js
@@ -555,10 +555,10 @@ Deno.serve(async (req) => {
             }
 
             if((new URLPattern({ pathname: "/api/notebooks" })).exec(req.url)) {
-                fetching = await fetch(`https://micro.blog/notes/notebooks`, { method: "GET", headers: { "Authorization": "Bearer " + mbToken } } );
+                const fetching = await fetch(`https://micro.blog/notes/notebooks`, { method: "GET", headers: { "Authorization": "Bearer " + mbToken } } );
                 const notebooks = await fetching.json();
-                //const data = notebooks.items.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0))
-                return new Response(JSON.stringify(notebooks.items), JSONHeaders());
+                const data = notebooks.items.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0))
+                return new Response(JSON.stringify(data), JSONHeaders());
             }
 
             if((new URLPattern({ pathname: "/api/following/favorites" })).exec(req.url)) {
