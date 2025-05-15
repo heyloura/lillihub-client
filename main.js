8ef48fe008f467963834b5769f57ae1a17f85810
heyloura <112587406+heyloura@users.noreply.github.com>
2025-05-15 12:52:39 -0400

    small image popup....

--- main.js
+++ main.js
@@ -2648,12 +2648,27 @@ function formatHTML (str) {
                 let parent;
                 image.classList.add('small-width');
                 image.classList.add('small-height');
+                image.setAttribute('data-ui', `#${image.getAttribute('src')}`);
                 if (image.parentNode.nodeName.toLowerCase() == "a"){
                     parent = image.parentNode;             
                 } else {
                     parent = image;
                 }
                 parents.push(parent);
+                const dialog = html.createElement('dialog');
+                dialog.classList.add('modal');
+                dialog.setAttribute('id', image.getAttribute('src'));
+                dialog.insertAdjacentHTML('beforeend', `<header class="fixed front">
+                    <nav>
+                        <div class="max ">
+                        </div>
+                        <button data-ui="#${image.getAttribute('src')}" class="circle transparent"><i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                            </svg>
+                        </i></button>
+                    </nav>
+                </header><img loading="lazy" src="${image.getAttribute('src')}"/>`)
             } else {
                 image.classList.add('single');
             }
