710d1d565183821eb48645c3970b0659556dcf9f
heyloura <112587406+heyloura@users.noreply.github.com>
2025-05-15 12:41:23 -0400

    hopefully convo fix

--- main.js
+++ main.js
@@ -2720,8 +2720,7 @@ async function getConversationHTML(id, token, nonce, csp) {
     let page = `<div id="conversation-${id}">${results.items.reverse().map((item,i) => {
             let heading = "<h6>Post</h6>";
             let subHeading = "<h6>COnversation</h6>";
-            item = flattenPost(item);
-            return `${i == 0 ? heading : ""}${i == 1 ? subHeading : ''}${postHTML(item,false)}`;
+            return `${i == 0 ? heading : ""}${i == 1 ? subHeading : ''}${postHTML(item,false,true)}`;
         }).join('')}
         ${replyForm(id, replyCheckboxes)}</div>`;
 
@@ -2799,7 +2798,7 @@ async function getMicroBlogUser(accessToken) {
     return result;
 }
 
-function postHTML(item, lastSeen) {
+function postHTML(item, lastSeen, convoView = false) {
     item = flattenPost(item);
     return `
         <article class="no-elevate round ${lastSeen ? 'secondary-container' : ''}" data-id="${item.id}">
@@ -2841,7 +2840,7 @@ function postHTML(item, lastSeen) {
                                             <li data-ui="#dialog-reply-${item.id}">Add Comment</li>
                                         </menu>  
                                     </button>
-                                    ${item.is_conversation ? 
+                                    ${item.is_conversation && !convoView ? 
                                         `<a href="/conversation/${item.id}" class="button transparent circle wave s"><i>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-text" viewBox="0 0 16 16">
                                                 <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
